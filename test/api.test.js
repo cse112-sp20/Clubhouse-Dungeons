34a00c3851a5d77c2eee150c180a93d7fdc5b506
Arren De Manuel <ademanue@ucsd.edu>
2020-05-18 14:54:21 -0700

    Found memberID

--- test/api.test.js
+++ test/api.test.js
@@ -1,10 +1,13 @@
 const api = require('../src/api/api');
 const testAPIToken = "5ec07d9a-f9a8-4541-a2fe-d2aae53169e1";
-const memberID = "";
+const memberID = "5ec07aaf-30c2-42ab-a9ee-7e1214f2e2d4";
 
 
 test('1 is 1', () => {
+    // Need to setup the API variables first
+    api.setup();
 
-    console.log(api.fetchMemberInfoAsync(testAPIToken));
+    // Then we will be testing this
+    api.getMyIncompleteStories();
     expect(1).toBe(1);
 });
