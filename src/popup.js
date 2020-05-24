97e0cb9cb7b0433a5117efc20abf61af8ad2b0d5
Matthew Kressin <matthew.d.kressin@gmail.com>
2020-05-24 09:21:31 -0700

    fix to pass linter  - missed unused imported function

--- src/popup.js
+++ src/popup.js
@@ -3,7 +3,7 @@ import {
   getMyIncompleteStories,
   getAllIncompleteStories,
   completeStoriesAsync,
-  revertCompleteStoriesAsync,
+  //  revertCompleteStoriesAsync,
   getBattleLog,
   getTopWarriors,
   getMemberName,
