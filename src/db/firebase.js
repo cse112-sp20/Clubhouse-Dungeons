ee26a906c36220d2800c92e59d49ae099941009b
Matthew Kressin <matthew.d.kressin@gmail.com>
2020-05-24 22:54:24 -0700

    adding use of firebase to our extension. added initial implementation of function to write user's data to our database

--- src/db/firebase.js
+++ src/db/firebase.js
@@ -1,6 +1,30 @@
 import firebase from 'firebase/app'
 import 'firebase/database'
 
+/**
+ * TYPE DECLARATIONS
+ *
+ * @typedef Database
+ * @type {Object}
+ * @property {Object} app The app assoiated with the Database service instance
+ * @see https://firebase.google.com/docs/reference/js/firebase.database.Database
+ */
+
+/**
+* Firebase configuration object
+* @type {
+    {
+      apiKey: string,
+      authDomain: string,
+      databaseURL: string,
+      projectId: string,
+      storageBucket: string,
+      messagingSenderId: number,
+      appId: string,
+      measurementId: string
+    }
+  }
+*/
 const firebaseConfig = {
   apiKey: 'AIzaSyDm1OKcCR9JjWRN1yxfVmiJfDpjBDIoiw0',
   authDomain: 'quaranteam-08.firebaseapp.com',
@@ -15,4 +39,34 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
 
+/**
+ * The reference to the Database service of our extension
+ *
+ * @const {Database}
+ */
 const database = firebase.database()
+
+/**
+ * Write the user's data to our extension's database
+ *
+ * @param {string} userId - the id of the user whose data we are storing
+ * @param {string} name - the user's name
+ * @param {boolean} [honored=false] - true if the user has been honored during this iteration, false otherwise
+ * @param {number} [timesHonored=0] - the number of times the user has been honored during this iteration
+ */
+const writeUserData = (userId, name, honored, timesHonored) => {
+  database.ref(`users/${userId}`).set({
+    username: name,
+    honored: honored,
+    timesHonored: timesHonored
+  },
+  /* callback function */
+  (error) => {
+    if (error) {
+      console.error(`writeUserData failed: ${error.message}`)
+    } else {
+      console.log('successfully wrote user data to database')
+    }
+  })
+}
+export { writeUserData }
