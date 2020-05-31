438333f4a86800f7b90f6ac47cd148fd8d555dd2
Kevin Montenegro <kemonten@ucsd.edu>
2020-05-30 21:12:20 -0700

    first integration test

---
+++ test/popup.test.js
@@ -0,0 +1,49 @@
+
+test('1 is 1', () => {
+    expect(1).toBe(1);
+});
+
+
+//const popup = require('../src/popup');
+//const popupAPIToken = "5ed23b63-23cb-49ab-920c-bd3b4c552635";
+//const memberID = "5ec07aaf-30c2-42ab-a9ee-7e1214f2e2d4";
+//const fetch = require("isomorphic-fetch").fetch;
+//const resolve = require("isomorphic-fetch").resolve;
+//const myName = '_Test User_';
+//const myIcon = 'https://cdn.patchcdn.com/assets/layout/contribute/user-default.png'
+//const profileContainer = popup.document.getElementById('profileContainer')
+
+///**
+// * Testing suite for toggleMemberMenu unit tests
+//  */
+//describe('toggleMemberMenu Unit tests', () => {
+
+//    /**
+//     * Unit Test:
+//     * Checks if toggleMemberMenu returns correct name
+//     */
+//    it('Testing when Closed', () => {
+//        if (profileContainer.classList.contains('open')) {
+//            profileContainer.classList.remove('open')
+//            profileContainer.classList.add('closed')
+//        }
+
+//        toggleMemberMenu();
+//        expect(profileContainer.classList).toContain('open')
+//    });
+
+//    /**
+//     * Unit Test:
+//     * Checks if getMemberProfile returns a correct default profile
+//     */
+//    it('Testing when Open', () => {
+//        if (profileContainer.classList.contains('closed')) {
+//            profileContainer.classList.remove('closed')
+//            profileContainer.classList.add('open')
+//        }
+//        toggleMemberMenu();
+//        expect(profileContainer.classList).toContain('closed');
+
+//    });
+
+//});
--- test/sample.test.js
+++ test/sample.test.js
@@ -1,19 +1,16 @@
+const extensionPath = '../dist'; // This is the path to the manifext.json file
+const extensionID = `cngkocoehccomngohodhpmlpekpdjppj`; // This extension ID might not work for you, but if it does, feel free to remove this comment :)
+const extensionPopupHtml = `login.html`; // The main page of our extension
 
-test('1 is 1', () => {
-    expect(1).toBe(1);
-});
-
-
-const myMock = jest.fn();
-myMock.mockReturnValueOnce(true).mockReturnValueOnce(false);
 const expectPuppeteer = require('expect-puppeteer');
 const puppeteer = require('puppeteer');
+var extensionPage;
 var browser;
-/*
+
 describe('extension', () => {
     beforeAll(async () => {
         // describe path to our extension
-        const pathToExtension = require('path').join(__dirname, '../src');
+        const pathToExtension = require('path').join(__dirname, '../dist');
         browser = await puppeteer.launch({
             headless: false,
             args: [
@@ -21,32 +18,25 @@ describe('extension', () => {
                 `--load-extension=${pathToExtension}`
             ]
         });
+
+        // Open the extension in its own tab
+        extensionPage = await browser.newPage();
+        await extensionPage.goto(`chrome-extension://${extensionID}/${extensionPopupHtml}`);
     });
 
-    it('browser opens and url == "about:blank"', async () => {
-
-        const targets = await browser.targets();
-
-
-        // ONLY TESTS BACKGROUND PROCESS
-        //const BackgroundPage = targets.find(target => target.type() === 'background_page');
-        //const page = await BackgroundPage.page();
-
-        //returns page of the browser - not the extension
-        const page = targets.find(target => target.type() === 'page');
-
-        await expect(page.url()).toMatch('about:blank');
-        // CANT TEST EXTENSION POPUPS!!!
-        // Can test background processes only
-
-        //await expect(page).toContain('Quaranteam - 8')
-        //await expect(page.title()).resolves.toMatch('Quaranteam - 8');
-        //await expect(browser).toMatch('')
-
+    afterAll(async () => {
         browser.close();
+
+    });
+
+
+    it('test for correct URL"', async () => {
+
+        await expect(extensionPage.url()).toMatch(`chrome-extension://${extensionID}/${extensionPopupHtml}`);
+
     });
 })
-*/
+
 
 //test('clicks the button', () => {
 //    buttonClick(myMock);
