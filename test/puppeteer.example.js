5af5b3a0d4f7f44f1c3cc156960b30db61a324a6
Arren <ademanue@ucsd.edu>
2020-05-30 18:03:38 -0700

    Set stable extension ID

--- test/puppeteer.example.js
+++ test/puppeteer.example.js
@@ -1,9 +1,9 @@
 const puppeteer = require('puppeteer');
 const assert = require('assert');
 
-const extensionPath = '../../../../../'; // This is the path to the manifext.json file
-const extensionID = `kggoidbofmfjedcmnincmhlefijkpgej`; // This extension ID might not work for you, but if it does, feel free to remove this comment :)
-const extensionPopupHtml = `popup.html`; // The main page of our extension
+const extensionPath = '../../../../../dist'; // This is the path to the manifext.json file
+const extensionID = `cngkocoehccomngohodhpmlpekpdjppj`; // This extension ID might not work for you, but if it does, feel free to remove this comment :)
+const extensionPopupHtml = `login.html`; // The main page of our extension
 
 // Puppeteer object variables here
 let extensionPage = null;
@@ -13,9 +13,9 @@ let browser = null;
  * Calling the testing function here.  It is an async method.
  * We are defining the async function and immediately calling it
  */
-/*
 (async () => {
 
+  console.log("test")
   // Open a new browser with puppeteer and load the extension
   browser = await puppeteer.launch({
     headless: false, // extension are allowed only in head-full mode
@@ -25,9 +25,7 @@ let browser = null;
     ]
   });
 
-
   // Open the extension in its own tab
   extensionPage = await browser.newPage();
   await extensionPage.goto(`chrome-extension://${extensionID}/${extensionPopupHtml}`);
 })();
-*/
