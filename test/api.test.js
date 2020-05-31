09fa3c0069b6848f3429667f0ed0d02554fc87d2
Chris_Desktop <cmaytom@ucsd.edu>
2020-05-31 00:40:42 -0700

    fixed linting error in api.test.js

--- test/api.test.js
+++ test/api.test.js
@@ -1,8 +1,13 @@
-const api = require('../src/api/api')
+import {
+  getMyIncompleteStories,
+  getAllIncompleteStories,
+  getMemberName,
+  getMemberProfile,
+  setupTest
+} from '../src/api/api'
+// import { fetch } from 'isomorphic-fetch'
 const testAPIToken = '5ec07d9a-f9a8-4541-a2fe-d2aae53169e1'
 const memberID = '5ec07aaf-30c2-42ab-a9ee-7e1214f2e2d4'
-const fetch = require('isomorphic-fetch').fetch
-const resolve = require('isomorphic-fetch').resolve
 
 const myIncompleteCount = 2
 const myIncompleteIDs = [56, 90]
@@ -21,9 +26,9 @@ it('Test MY Incomplete Stories', done => {
     * Chase mentioned that we need to set up a test method in api
     * So I went ahead and defined a method called setupTest in api.js
     */
-  api.setupTest(testAPIToken, memberID, () => {
+  setupTest(testAPIToken, memberID, () => {
     // The only story that has been assigned to this test user as of right now is story 56
-    incompleteStories = api.getMyIncompleteStories()
+    const incompleteStories = getMyIncompleteStories()
     expect(incompleteStories.length).toBe(myIncompleteCount) // Make sure the number of incomplete is correct
     expect(myIncompleteIDs).toContain(incompleteStories[0].id) // Then check if the values for these incomplete stories are also correct
     expect(myIncompleteIDs).toContain(incompleteStories[1].id)
@@ -41,9 +46,9 @@ it('Test ALL Incomplete Stories', done => {
     * Chase mentioned that we need to set up a test method in api
     * So I went ahead and defined a method called setupTest in api.js
     */
-  api.setupTest(testAPIToken, memberID, () => {
+  setupTest(testAPIToken, memberID, () => {
     // The only story that has been assigned to this test user as of right now is story 56
-    incompleteStories = api.getAllIncompleteStories()
+    const incompleteStories = getAllIncompleteStories()
 
     const allStories = []
 
@@ -74,8 +79,8 @@ describe('api simple unit tests', () => {
   it('Testing getMemberName', () => {
     var name
     // test setup - tried in beforeAll/beforeEach block, can't get it to work
-    api.setupTest(testAPIToken, memberID, () => {
-      name = api.getMemberName()
+    setupTest(testAPIToken, memberID, () => {
+      name = getMemberName()
       expect(name).toMatch(myName)
       done()
     })
@@ -88,8 +93,8 @@ describe('api simple unit tests', () => {
   it('Testing default getMemberProfile', () => {
     var profile
 
-    api.setupTest(testAPIToken, memberID, () => {
-      profile = api.getMemberProfile()
+    setupTest(testAPIToken, memberID, () => {
+      profile = getMemberProfile()
       expect(profile.name).toMatch(myName)
       expect(profile.icon).toContain(myIcon)
       done()
