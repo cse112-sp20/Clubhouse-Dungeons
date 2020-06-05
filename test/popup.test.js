aac9d393e6bef23d05d5d95fbe49571361f9b8e5
Kevin Montenegro <kemonten@ucsd.edu>
2020-06-04 19:07:00 -0700

    First Integration tests -error

--- test/popup.test.js
+++ test/popup.test.js
@@ -1,49 +1,85 @@
+const extensionPath = '../dist'; // This is the path to the manifext.json file
+const extensionID = `cngkocoehccomngohodhpmlpekpdjppj`; // This extension ID might not work for you, but if it does, feel free to remove this comment :)
+const extensionLoginHtml = `login.html`; // The main page of our extension
+const extensionPopupHtml = `popup.html`;
 
-test('1 is 1', () => {
-    expect(1).toBe(1);
-});
+const testAPIToken = '5ed2b278-d7a6-4344-b33f-94b8901aa75a'
+const memberID = '5ecdd3de-0125-4888-802a-5d3ba46ca0dc'
+const workspace = 'quarantest8'
+const myName = '_Test User_'
+const waitForNavigationTimeout = 3000;
+const expectPuppeteer = require('expect-puppeteer');
+const puppeteer = require('puppeteer');
+var extensionPage;
+var browser;
 
+describe('Login intended behavior', () => {
 
-//const popup = require('../src/popup');
-//const popupAPIToken = "5ed23b63-23cb-49ab-920c-bd3b4c552635";
-//const memberID = "5ec07aaf-30c2-42ab-a9ee-7e1214f2e2d4";
-//const fetch = require("isomorphic-fetch").fetch;
-//const resolve = require("isomorphic-fetch").resolve;
-//const myName = '_Test User_';
-//const myIcon = 'https://cdn.patchcdn.com/assets/layout/contribute/user-default.png'
-//const profileContainer = popup.document.getElementById('profileContainer')
+    // Before anything open a new browser with our extension enabled
+    beforeAll(async () => {
+        // describe path to our extension
+        const pathToExtension = require('path').join(__dirname, '../dist');
+        //open a new browser
+        browser = await puppeteer.launch({
+            headless: false,
+            args: [
+                `--disable-extensions-except=${pathToExtension}`,
+                `--load-extension=${pathToExtension}`
+            ]
+        });
 
-///**
-// * Testing suite for toggleMemberMenu unit tests
-//  */
-//describe('toggleMemberMenu Unit tests', () => {
+        extensionPage = await browser.newPage();
+        await extensionPage.goto(`chrome-extension://${extensionID}/${extensionLoginHtml}`);
 
-//    /**
-//     * Unit Test:
-//     * Checks if toggleMemberMenu returns correct name
-//     */
-//    it('Testing when Closed', () => {
-//        if (profileContainer.classList.contains('open')) {
-//            profileContainer.classList.remove('open')
-//            profileContainer.classList.add('closed')
-//        }
+        await extensionPage.type('#apiEntry', testAPIToken);
 
-//        toggleMemberMenu();
-//        expect(profileContainer.classList).toContain('open')
-//    });
+        // click the button then wait for navigation
+        await Promise.all([
+            extensionPage.waitForNavigation(), // The promise resolves after navigation has finished
+            extensionPage.click('#button')
+        ]).catch((error) => {
+            fail('Login button not correctly redirecting')
+        });
 
-//    /**
-//     * Unit Test:
-//     * Checks if getMemberProfile returns a correct default profile
-//     */
-//    it('Testing when Open', () => {
-//        if (profileContainer.classList.contains('closed')) {
-//            profileContainer.classList.remove('closed')
-//            profileContainer.classList.add('open')
-//        }
-//        toggleMemberMenu();
-//        expect(profileContainer.classList).toContain('closed');
+        await extensionPage.close();
+    });
 
-//    });
+    // Before each test, open our extension in a new page
+    beforeEach(async () => {
+        extensionPage = await browser.newPage();
+        await extensionPage.goto(`chrome-extension://${extensionID}/${extensionLoginHtml}`);
+        await extensionPage.waitForNavigation();
+    });
 
-//});
+    // After each test, close the page
+    afterEach(async () => {
+        await extensionPage.close();
+    });
+
+    // After all tests are done, close the browser
+    afterAll(async () => {
+        browser.close();
+    });
+
+    /**
+    * Unit Test 0
+    * Testing opened page in browser
+    * Checks if new page correctly redirects to PopupHTML
+    */
+    it('Test for correct URL/Redirect', async () => {
+
+        expect(extensionPage.url()).toMatch(`chrome-extension://${extensionID}/${extensionPopupHtml}`);
+        //await expect(extensionPage.url()).toMatch('wrong');
+
+    });
+
+    /**
+     * Unit Test 1
+     */
+    it('Test "All Stories" Tab button', async () => {
+        await extensionPage.click('#profileContainer');
+        const profileContainer = extensionPage.$('#profileContainer');
+        expect(profileContainer).toContain('open');
+    });
+
+})
