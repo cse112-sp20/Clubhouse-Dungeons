50a819c2baa2d31f0c624a7e2ee954c0dfdd07a8
Chase Sriprajittichai <sriprajittichai@gmail.com>
2020-06-08 18:06:08 -0700

    Use Quarantest8 testing iteration in unit tests

--- src/api/clubhouse-api.js
+++ src/api/clubhouse-api.js
@@ -52,6 +52,7 @@
 
 /**
  * @typedef {object} Iteration - Iteration object that contains all info about an iteration from Clubhouse
+ * @property {number} id - ID of the iteration
  * @property {Date} start_date - The starting date of the iteration
  * @property {Date} end_date - The ending date of the iteration
  * @property {string} status - The current status of the iteration. Can be 'started', 'unstarted' or 'done'
